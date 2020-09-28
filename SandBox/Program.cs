afa0cd61a4ee30468d13970eabf76085fe0a90af
CeSiumUA <46378315+CeSiumUA@users.noreply.github.com>
2020-09-28 17:30:27 +0300

    Update Program.cs

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -8,39 +8,7 @@ namespace SandBox
     {
         static void Main(string[] args)
         {
-            var c = new C();
-            A a = c;
-
-            a.Print2();
-            a.Print1();
-            c.Print2();
-
-            Console.ReadLine();
-        }
-    }
-    public class A
-    {
-        public virtual void Print1()
-        {
-            Console.Write("A");
-        }
-        public void Print2()
-        {
-            Console.Write("A");
-        }
-    }
-    public class B : A
-    {
-        public override void Print1()
-        {
-            Console.Write("B");
-        }
-    }
-    public class C : B
-    {
-        new public void Print2()
-        {
-            Console.Write("C");
+            
         }
     }
 }
